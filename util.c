79045a5801612a1503d7234943d97f3ba7254fe2
Oleksandr Zolotarov <oleksandr.zolotarov@harmonicinc.com>
2023-12-09 12:56:30 +0200

    utils

---
+++ util.c
@@ -0,0 +1,6 @@
+#include "util.h"
+
+int min(int val1, int val2)
+{
+    return val1 > val2 ? val2 : val1;
+}
---
+++ util.h
@@ -0,0 +1,6 @@
+#ifndef UTIL_H_
+#define UTIL_H_
+
+int min(int val1, int val2);
+
+#endif //UTIL_H_
